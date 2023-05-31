6644660afd2ae9c2927222ba2afa025cdb73e957
Ramón Diaz <ramon.diazguerra@gmail.com>
2023-05-31 11:44:40 +0200

    Ejercicio 1

---
+++ src/componentes/escena/Escena.tsx
@@ -0,0 +1,12 @@
+
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function Welcome() {
+  return (
+    <h1>
+      El nostre heroi  estavaaaa surant per l'espai  sideral quan a la llunyania va
+      albirar una nau espacial
+    </h1>
+  );
+}
+export default Welcome;

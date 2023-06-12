dffc182bc882ab704b3d4c01a892a2068365d50d
Ramón Diaz <ramon.diazguerra@gmail.com>
2023-06-12 21:22:33 +0200

    Ejercicio 5

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,12 @@ import { useState } from "react";
 //Solo se pueden usar los HOOKS dentro de function components, no dentro de CLASES
 //USESTATE no se puede llamar dentro de un condicional
 function App() {
+  //Declaramos estado para mostrar pantalla de bienvenida:
+  const [welcomeScreen, showWelcomeScreen] = useState<boolean>(true);
+  const ocultarPantalla = () => {
+    showWelcomeScreen(false);
+  }
+  //Declaro estado para asignarle valor a cada uno de los pasos de la historia segun pulsemos los botones:
   const [position, setPosition] = useState<number>(1);
 
   const mueveAtras=()=>{
@@ -24,6 +30,7 @@ function App() {
       //console.log(position);
     }
   }
+
    const mueveAdelante=()=> {
     if (position < data.length) {
       setPosition(position + 1);
@@ -33,24 +40,51 @@ function App() {
       setPosition(1);
       //console.log(position);
     }
+   }
+  if (welcomeScreen) {
+    return (
+      <>
+        <div
+          style={{
+            display: "flex-column",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "orange",
+            width:"100%",
+          }}
+        >
+          <h3>
+            Proyecto de desarrollo de tutorial mediante botones que permitan
+            avanzar y retroceder en la historia
+          </h3>
+          <button onClick={ocultarPantalla}>Comenzar</button>
+        </div>
+      </>
+    );
   }
-  //La propiedad "key" es un atributo especial  que se debe incluir al crear listas de elementos. Cada key debe ser único por lista.
-  return (
-    <>
-      <div>
-        <button onClick={mueveAtras} className="boton">
-          Anterior
-        </button>
-        <button onClick={mueveAdelante} className="boton">
-          Siguiente
-        </button>
-      </div>{" "}
-
-      {data.map((frase) => (
-        <Escena  key={frase.id} id={frase.id} sentence={frase.texto} position={position}  /> //le pasamos mediante props el state (position) que tenemos en cada momento.
-      ))}
-    </>
-  );
+  else {
+    //La propiedad "key" es un atributo especial  que se debe incluir al crear listas de elementos. Cada key debe ser único por lista.
+    return (
+      <>
+        <div>
+          <button onClick={mueveAtras} className="boton">
+            Anterior
+          </button>
+          <button onClick={mueveAdelante} className="boton">
+            Siguiente
+          </button>
+        </div>
+        {data.map((frase) => (
+          <Escena
+            key={frase.id}
+            id={frase.id}
+            sentence={frase.texto}
+            position={position}
+          /> //le pasamos mediante props el state (position) que tenemos en cada momento.
+        ))}
+      </>
+    );
+  }
 }
 
 export default App;
